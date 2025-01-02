66a5fccf87d6c00fe1cece8986abaf220e998b27
LACHKAR Zakaria <lachkar99zakaria@gmail.com>
2025-01-02 13:59:02 +0100

    Initial Commit

---
+++ Calendries Teams/Calendries_TeamsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calendries_TeamsApp.swift
+//  Calendries Teams
+//
+//  Created by Zakaria Lachkar on 2/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct Calendries_TeamsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
